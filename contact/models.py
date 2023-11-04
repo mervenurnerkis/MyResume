6e08722bd47da622fbda3bca8d0d3ba7a482554e
mervenurnerkis <nerkis1999@gmail.com>
2023-11-04 19:54:51 +0300

    feat: create message model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from core.models import AbstractModel
 # Create your models here.
+
+class Message(AbstractModel):
+    name = models.CharField(
+        max_length=254,
+        blank=True,
+        verbose_name='Name',
+    )
+    email = models.EmailField(
+        max_length=254,
+        blank=True,
+        verbose_name='Email',
+    )
+    subject = models.CharField(
+        max_length=254,
+        blank=True,
+        verbose_name='Subject',
+    )
+    message = models.TextField(
+        blank=True,
+        verbose_name='Message',
+    )
+
+    def __str__(self):
+        return f'Message: {self.name}'
--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,31 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from contact.models import Message
 # Create your views here.
 
 def contact_form(request):
+    if request.POST:
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        Message.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
+        )
+
+        success = True
+        message = 'Contact form sent successfully.'
+    else:
+        success = False
+        message = 'Request method is not valid.'
+
     context = {
-        'success': True,
-        'message': 'Contact form sent successfully.'
+        'success': success,
+        'message': message
     }
     return JsonResponse(context)
 
